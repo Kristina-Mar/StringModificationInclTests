8b83a92c88ee952fdc8699202ada431774b587c2
Kristina Martiskova <kris.martiskova@gmail.com>
2024-02-24 15:02:52 +0100

    Add project files.

---
+++ StringModification.Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+namespace StringModification.Tests
+{
+    public class UnitTest1
+    {
+        StringModificationMethods stringMethods = new StringModificationMethods();
+
+        [Theory]
+        [InlineData("Alabama", "amabalA")]
+        [InlineData("I show not your face but your heart's desire.", ".erised s'traeh ruoy tub ecaf ruoy ton wohs I")]
+        public void TestBackwordsWord(string originalString, string expected)
+        {
+            string backwordsString = stringMethods.StringBackwards(originalString);
+            Assert.Equal(expected, backwordsString);
+
+            backwordsString = stringMethods.StringBackwards(originalString);
+            Assert.Equal(expected, backwordsString);
+        }
+        [Theory]
+        [InlineData("Alabama", "alabama")]
+        [InlineData("I show not your face but your heart's desire.", "i show not your face but your heart's desire.")]
+        public void TestAllLowerCase(string originalString, string expected)
+        {
+            string allLowerCaseString = stringMethods.StringToAllLowerCase(originalString);
+            Assert.Equal(expected, allLowerCaseString);
+
+            allLowerCaseString = stringMethods.StringToAllLowerCase(originalString);
+            Assert.Equal(expected, allLowerCaseString);
+        }
+        [Theory]
+        [InlineData("Alabama", "ALABAMA")]
+        [InlineData("I show not your face but your heart's desire.", "I SHOW NOT YOUR FACE BUT YOUR HEART'S DESIRE.")]
+        public void TestAllUpperCase(string originalString, string expected)
+        {
+            string allUpperCaseString = stringMethods.StringToAllCaps(originalString);
+            Assert.Equal(expected, allUpperCaseString);
+
+            allUpperCaseString = stringMethods.StringToAllCaps(originalString);
+            Assert.Equal(expected, allUpperCaseString);
+        }
+    }
+}
---
+++ StringModification/StringModificationMethods.cs
@@ -0,0 +1,39 @@
+﻿namespace StringModification
+{
+    public class StringModificationMethods
+    {
+        public string StringBackwards(string originalString)
+        {
+            char[] stringArray = originalString.ToCharArray();
+            char[] backwordsWordArray = new char[stringArray.Length];
+            int backwordsStringArrayIndex = 0;
+            for (int i = stringArray.Length - 1; i >= 0; i--)
+            {
+                backwordsWordArray[backwordsStringArrayIndex] = stringArray[i];
+                backwordsStringArrayIndex++;
+            }
+            string backwordsString = new string(backwordsWordArray);
+            return backwordsString;
+        }
+        public string StringToAllLowerCase(string originalString)
+        {
+            char[] stringArray = originalString.ToCharArray();
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                stringArray[i] = Char.ToLower(stringArray[i]);
+            }
+            string allLowerCaseString = new string(stringArray);
+            return allLowerCaseString;
+        }
+        public string StringToAllCaps(string originalString)
+        {
+            char[] stringArray = originalString.ToCharArray();
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                stringArray[i] = Char.ToUpper(stringArray[i]);
+            }
+            string allUpperCaseString = new string(stringArray);
+            return allUpperCaseString;
+        }
+    }
+}
